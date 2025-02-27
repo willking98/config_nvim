c9aee1fc14af2aec2910c4c9d886982ced2aaf7d
Will King <64603291+willking98@users.noreply.github.com>
2025-02-27 21:38:31 +0000

    markdown viewing update

--- lua/kickstart/plugins/markdown_nvim.lua
+++ lua/kickstart/plugins/markdown_nvim.lua
@@ -5,5 +5,40 @@ return {
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = {},
+  opts = {
+    heading = {
+      width = 'block',
+      min_width = 50,
+      border = true,
+      backgrounds = {
+        'RenderMarkdownH1Bg',
+        'RenderMarkdownH2Bg',
+        'RenderMarkdownH3Bg',
+        'RenderMarkdownH4Bg',
+        'RenderMarkdownH5Bg',
+        'RenderMarkdownH6Bg',
+      },
+      foregrounds = {
+        'RenderMarkdownH1',
+        'RenderMarkdownH2',
+        'RenderMarkdownH3',
+        'RenderMarkdownH4',
+        'RenderMarkdownH5',
+        'RenderMarkdownH6',
+      },
+    },
+    render_modes = { 'n', 'v', 'i', 'c' },
+    checkbox = {
+
+      unchecked = { icon = '󰄱 ' },
+      checked = { icon = ' ' },
+      custom = { todo = { raw = '[>]', rendered = '󰥔 ' } },
+    },
+    code = {
+      position = 'right',
+      width = 'block',
+      left_pad = 2,
+      right_pad = 4,
+    },
+  },
 }
